d9e873964d11b125bab7470f0b7c9e72078ab630
ferdy <xv.ferdy@gmail.com>
2020-09-29 03:43:21 +0700

    :lipstick: Styling

--- src/Doglist.js
+++ src/Doglist.js
@@ -6,13 +6,15 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1">Dog List!</h1>
+        <h1 className="display-1 mt-4 mb-4">Dog List!</h1>
         <div className="row">
           {this.props.dogs.map((dog) => (
             <div className="Dog col-lg-4" key={dog.name}>
               <img src={dog.src} alt={dog.name} />
-              <h3>
-                <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+              <h3 className="mt-3">
+                <Link className="underline" to={`/dogs/${dog.name}`}>
+                  {dog.name}
+                </Link>
               </h3>
             </div>
           ))}
